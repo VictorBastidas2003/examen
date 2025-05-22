14eac920ba3674d6f3e78ac3368840acf5d686a9
Victor <vbastidas963@gmail.com>
2025-05-22 18:40:05 -0500

    Primer commit - subida inicial del proyecto

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { HomePage } from './home.page'; // 👈 Import del componente standalone
 import { HomePageRoutingModule } from './home-routing.module';
-
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+    HomePageRoutingModule,
+    HomePage 
+  ]
 })
 export class HomePageModule {}
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
 })
 export class HomePage {
+  registroForm: FormGroup;
+  submitted = false;
+  datos: any = null;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmarPassword: ['', Validators.required]
+    }, { validators: this.passwordsIguales });
+  }
 
+  passwordsIguales(control: AbstractControl): { [key: string]: boolean } | null {
+    const password = control.get('password')?.value;
+    const confirmarPassword = control.get('confirmarPassword')?.value;
+    return password === confirmarPassword ? null : { passwordMismatch: true };
+  }
+
+  enviarFormulario() {
+    this.submitted = true;
+    if (this.registroForm.valid) {
+      this.datos = this.registroForm.value;
+    }
+  }
 }
